04a2cb8e086fb8e2ceca5447ed24c153e306bfd0
hamsssa <hamzabenfilahi@gmail.com>
2025-02-13 20:47:25 +0100

    Create setAlarm.js

---
+++ setAlarm.js
@@ -0,0 +1,20 @@
+function setAlarm(employed, vacation)
+{
+  //Recibo dos parametros
+  //Employed es true cada que estoy employed? xd wtf
+  //Vacation es true cuando estoy de vacaciones xd? 
+  
+  //La funcion tiene que ser TRUE si estas employed y no de vacaciones 
+  //Else return false
+
+  
+  if(employed == true && vacation == false)
+    {
+      return true;
+    }
+  else
+    {
+      return false;
+    }
+
+ }
