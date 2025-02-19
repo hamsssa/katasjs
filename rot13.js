d34fc8d527e14a37fa3dee250468da3cc0ebd21f
hamsssa <hamzabenfilahi@gmail.com>
2025-02-19 03:28:47 +0100

    Update rot13.js

--- rot13.js
+++ rot13.js
@@ -4,7 +4,7 @@ function rot13(message)
     let newArr = [];
     
     for (let i = 0; i < array.length; i++) 
-    { // Corregir condición del for
+    { 
         let char = array[i];
         let code = char.charCodeAt(0);
         
@@ -12,17 +12,17 @@ function rot13(message)
         if (/^[A-Za-z]$/.test(char)) 
         { 
             if ((code >= 65 && code <= 90)) 
-            { // Mayúsculas
+            { 
                 code = ((code - 65 + 13) % 26) + 65;
             } 
           else 
-            { // Minúsculas
+            { /
                 code = ((code - 97 + 13) % 26) + 97;
             }
             newArr.push(String.fromCharCode(code));
         } else 
         {
-            newArr.push(char); // Mantener caracteres no alfabéticos
+            newArr.push(char);
         }
     }
     
