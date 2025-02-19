9b25bce3b23f2e946e4377ac14615be2a9eb3944
hamsssa <hamzabenfilahi@gmail.com>
2025-02-19 03:27:06 +0100

    Create isPangram.js

---
+++ isPangram.js
@@ -0,0 +1,8 @@
+function isPangram(string)
+{
+  let s = string.toLowerCase();
+  let abecedario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  
+    return abecedario.every(letters => s.includes(letters));
+
+}
